7c5871df1d8a7a912218fc950643fe645af0f4b9
MCarbono <carbono.marcelo@gmail.com>
2023-07-19 23:01:06 -0300

    removed unnecessary comments

--- infra/database/postgres.go
+++ infra/database/postgres.go
@@ -20,16 +20,6 @@ func (cfg PostgresConfig) string() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
 }
 
-// func DefaultPostgresConfig() PostgresConfig {
-// 	return PostgresConfig{
-// 		Host:     os.Getenv("PSQL_HOST"),
-// 		Port:     os.Getenv("PSQL_PORT"),
-// 		User:     os.Getenv("PSQL_USER"),
-// 		Password: os.Getenv("PSQL_PASSWORD"),
-// 		Database: os.Getenv("PSQL_DATABASE"),
-// 		SSLMode:  os.Getenv("PSQL_SSL_MODE"),
-// 	}
-// }
 func NewPostgresConfig(host, port, user, password, database, sslmode string) PostgresConfig {
 	return PostgresConfig{
 		Host:     host,
